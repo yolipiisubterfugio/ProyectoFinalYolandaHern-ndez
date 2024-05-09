edc43aee469c79d3d4b7125846900beaa2a9ee48
Yolanda Hernandez <yolanda_016@hotmail.com>
2024-05-09 20:58:35 +0200

    adding sql file

---
+++ src/main/resources/templates/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO alumno (nombre,apellido,email,dni,telefono) VALUE ('Jose','Perez','jp@email.com','A245454',6124512);
+INSERT INTO alumno (nombre,apellido,email,dni,telefono) VALUE ('Luis','Salinas','ls@email.com','P98237',6524584);
+INSERT INTO alumno (nombre,apellido,email,dni,telefono) VALUE ('Maria','Gomez','mg@email.com','R245454',9245454);
+INSERT INTO alumno (nombre,apellido,email,dni,telefono) VALUE ('Gustavo','Caceres','gc@email.com','Y245454',6877512);
